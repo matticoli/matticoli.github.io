f9e89088893ba9887b4b7c52839561b4c0a30c1a
Mikel Matticoli <mikelmm99@gmail.com>
2017-02-19 13:33:07 -0500

    Going live
    - reorganized categories
    - updated links and contact info
    - moved content to subfolder
    -tested on different screen sizes

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ $('.category').click( function(event) {
     var id = $(this).attr("id");
     var proj = $('.projects');
     proj.fadeOut(250, function() {
-        proj.load(id+".html", function () {
+        proj.load("./content/"+id+".html", function () {
             proj.fadeIn(250);
         });
     });
