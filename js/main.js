c4b9488d3a23b19ef7b780c19fe897244abaf316
Mikel Matticoli <mikel.matticoli@gmail.com>
2018-05-24 23:55:44 -0400

    page load/unload animations

--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
+let slideout = () => {
+    let tiles = $('.button-top').toArray().concat($('.button-bottom').toArray());//$('[data-button]').toArray();
+    for (let key in tiles) {
+        // if($(tiles[key]).hasClass("button-top")) {
+        //     $($(tiles[key])).css("transform", "translate(-100vw)");
+        // } else {
+        //     $($(tiles[key])).css("transform", "translate(100vw)");
+        // }
+        switch ($(tiles[key]).attr("data-button")) {
+            case "linkedin":
+                $($(tiles[key])).css("transform", "translate(-40vw)");
+                break;
+            case "github":
+                $($(tiles[key])).css("transform", "translate(-80vw)");
+                break;
+            case "resume":
+                $($(tiles[key])).css("transform", "translate(-100vw)");
+                break;
+            default:
+                $($(tiles[key])).css("transform", "translate(100vw)");
+                break;
+        }
+    }
+};
+
+let slidein = () => {
+    let tiles = $('[data-button]').toArray();
+    for (let key in tiles) {
+        setTimeout
+        $($(tiles[key])).css("transform", "");
+
+    }
+};
+
+
 $(document).ready(function () {
-            $("[data-js-page]").on("touchend", function () {
-                $(".button").css({
-                    "transform": ""
-                });
+    $('body').fadeOut(0);
+    slideout();
+    setTimeout(() => {
+        $('body').fadeIn(200);
+        slidein();
+    }, 300);
 
 
-                $("[data-js-button]").on("touchend", function () {
-                    var goal_menu = $(this).data("js-menu");
-                    $(this).css("transform", "translate(-200%)");
-                    $(".button[data-button='" + goal_menu + "']").css("transform", "translate(0%)");
-                });
+    $('a').click( (event) => {
+        event.preventDefault();
+        const newLoc = event.currentTarget.href;
+        slideout();
+        setTimeout( ()=> {
+            $('body').fadeOut(200);
+            window.location = newLoc;
+        }, 300);
+    });
+});
 
-            });
+
